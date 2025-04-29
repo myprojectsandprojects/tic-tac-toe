d18e45542e17ec8f8f8c2ac76e41e079a7b97ae5
Eero <eerolillemae@gmail.com>
2025-04-30 00:30:44 +0300

    making moves

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,6 @@
+
+//@ mouse coordinates are off when the mouse hasn't moved yet...
+
 const std = @import("std");
 const ray = @cImport(@cInclude("raylib.h"));
 
@@ -9,6 +12,7 @@ pub fn main() !void {
     ray.InitWindow(windowWidth, windowHeight, "Tic Tac Toe");
     defer ray.CloseWindow();
 
+    //ray.SetTargetFPS(1);
     ray.SetTargetFPS(60);
 
     const boardColor: ray.Color = ray.DARKBROWN;
@@ -22,18 +26,60 @@ pub fn main() !void {
     const cellHeight = boardHeight / 3;
 
     var board = [1]i8{0} ** 9; // -1 (white), 0 (empty), 1 (black)
-    board[0] = 1;
-    board[board.len-1] = -1;
-    board[2] = 1;
-    board[3] = -1;
-    for(board, 0..) |cell, i| {
-        const x = i % cellsInRow;
-        const y = i / cellsInRow;
-        print("{} (x: {}, y: {}): {}\n", .{i, x, y, cell});
-    }
-    print("\n", .{});
+    //board[0] = 1;
+    //board[board.len-1] = -1;
+    //board[2] = 1;
+    //board[3] = -1;
+    //for(board, 0..) |cell, i| {
+    //    const x = i % cellsInRow;
+    //    const y = i / cellsInRow;
+    //    print("{} (x: {}, y: {}): {}\n", .{i, x, y, cell});
+    //}
+    //print("\n", .{});
+
+    //for (0..10) |n| {
+    //    if (ray.IsMouseButtonDown(@intCast(n))) {
+    //        print("{}\n", .{n});
+    //    }
+    //}
+    const LEFT_MOUSE = 0;
+    //const RIGHT_BUTTON = 1;
+    var wasDown: bool = ray.IsMouseButtonDown(@intCast(LEFT_MOUSE));
+
+    var turn: i8 = 1;
 
     while (!ray.WindowShouldClose()) {
+        if (ray.IsMouseButtonDown(@intCast(LEFT_MOUSE))) {
+            if (wasDown == false) {
+                const x = ray.GetMouseX();
+                const y = ray.GetMouseY();
+                print("mouse went down at ({}, {})\n", .{x, y});
+                if (x >= boardX and x < boardX + boardWidth and y >= boardY and y < boardY + boardWidth) {
+                    const bx = @divFloor((x - boardX), cellWidth);
+                    const by = @divFloor((y - boardY), cellHeight);
+                    print("board: x: {}, y: {}\n", .{bx, by});
+                    const index: usize = @intCast(by * cellsInRow + bx);
+                    board[index] = turn;
+                    turn = if (turn == 1) -1 else 1;
+                }
+            }
+
+            wasDown = true;
+        } else {
+            wasDown = false;
+        }
+
+        //if (ray.IsMouseButtonDown(@intCast(RIGHT_BUTTON))) {
+        //    print("right down\n", .{});
+        //}
+
+        //if (ray.IsMouseButtonPressed(@intCast(LEFT_BUTTON))) {
+        //    print("left pressed\n", .{});
+        //}
+        //if (ray.IsMouseButtonPressed(@intCast(RIGHT_BUTTON))) {
+        //    print("right pressed\n", .{});
+        //}
+
         ray.BeginDrawing();
         ray.ClearBackground(ray.BEIGE);
 
