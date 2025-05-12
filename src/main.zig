82d6c88a88ca8bafe57edbbb6da81f16bd31e9d6
Eero <eerolillemae@gmail.com>
2025-05-12 10:54:42 +0300

    Can play now

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,12 @@
 
+// todo:
+// - who won? (black? white?)
+// - highlight the winning row in some way (make stones bigger) 
+// - play sounds when game ends (win/loss, draw)
+// - play sounds when a move is made / stone placed
+// - who's turn? 
+// - consider different board type (place stones at the grid-line intersections, larger board)
+
 //@ mouse coordinates are off when the mouse hasn't moved yet...
 
 const std = @import("std");
@@ -6,6 +14,12 @@ const ray = @cImport(@cInclude("raylib.h"));
 
 const print = std.debug.print;
 const assert = std.debug.assert;
+
+const Stone = enum {
+    BLACK,
+    WHITE,
+};
+const stoneToString = [2][]const u8{"BLACK", "WHITE"};
 
 pub fn main() !void {
     const windowWidth = 800;
@@ -16,79 +30,84 @@ pub fn main() !void {
     //ray.SetTargetFPS(1);
     ray.SetTargetFPS(60);
 
-    const boardColor: ray.Color = ray.DARKBROWN;
-    const boardX = 100;
-    const boardY = 100;
-    const boardWidth = 300;
-    const boardHeight = 300;
+    const boardColor = ray.DARKBROWN;
+    const boardX: f32 = 100;
+    const boardY: f32 = 100;
+    const boardWidth: f32 = 300;
+    const boardHeight: f32 = 300;
 
-    const columnsInRow = 3;
-    const cellWidth = boardWidth / 3;
-    const cellHeight = boardHeight / 3;
+    const numColumns = 3;
+    const numRows = 3;
+    const cellWidth: f32 = boardWidth / 3;
+    const cellHeight: f32 = boardHeight / 3;
 
     //var board: [3][3]?Stone; //?
-    var board = [1]i8{0} ** 9; // -1 (white), 0 (empty), 1 (black)
-    var numMovesMade: u8 = 0; 
+    var board: [9]?Stone = [1]?Stone{null} ** 9;
+    var numEmptyCells: u8 = board.len; 
     var gameOver: bool = false;
-    //board[0] = 1;
-    //board[board.len-1] = -1;
-    //board[2] = 1;
-    //board[3] = -1;
-    //for(board, 0..) |cell, i| {
-    //    const x = i % cellsInRow;
-    //    const y = i / cellsInRow;
-    //    print("{} (x: {}, y: {}): {}\n", .{i, x, y, cell});
-    //}
-    //print("\n", .{});
+    var gameOverString: ?[]const u8 = null;
 
-    //for (0..10) |n| {
-    //    if (ray.IsMouseButtonDown(@intCast(n))) {
-    //        print("{}\n", .{n});
-    //    }
-    //}
     const LEFT_MOUSE = 0;
     //const RIGHT_BUTTON = 1;
-    var wasDown: bool = ray.IsMouseButtonDown(@intCast(LEFT_MOUSE));
+    var wasDown: bool = ray.IsMouseButtonDown(LEFT_MOUSE);
 
-    var turn: i8 = 1;
+    var turn: Stone = .BLACK;
 
     while (!ray.WindowShouldClose()) {
-        if (!gameOver) {
-            if (ray.IsMouseButtonDown(@intCast(LEFT_MOUSE))) {
-                if (wasDown == false) {
-                    const x = ray.GetMouseX();
-                    const y = ray.GetMouseY();
-                    if (x >= boardX and x < boardX + boardWidth and y >= boardY and y < boardY + boardWidth) {
-                        const bx = @divFloor((x - boardX), cellWidth);
-                        const by = @divFloor((y - boardY), cellHeight);
-                        assert(bx >= 0 and by >= 0);
-                        const index: usize = @intCast(by * columnsInRow + bx);
-                        if (board[index] == 0) {
-                            // Make a move
-                            board[index] = turn;
-                            numMovesMade += 1;
+        const isDown = ray.IsMouseButtonDown(LEFT_MOUSE);
+        const mouseWentDown = isDown and !wasDown;
+        wasDown = isDown;
 
-                            if (isWin(board[0..], @intCast(bx), @intCast(by), columnsInRow)) {
-                                gameOver = true;
-                                print("WIN\n", .{});
-                                //for (&board) |*cell| {
-                                //    cell.* = 0;
-                                //}
-                            } else if (numMovesMade == 9) {
-                                gameOver = true;
-                                print("DRAW\n", .{});
-                            } else {
-                                turn = if (turn == 1) -1 else 1;
-                            }
+        if (!gameOver) {
+            if (mouseWentDown) {
+                const x = ray.GetMouseX();
+                const y = ray.GetMouseY();
+                if (x >= boardX and x < boardX + boardWidth and y >= boardY and y < boardY + boardWidth) {
+                    const bx = @as(u8, @intFromFloat(@floor((@as(f32, @floatFromInt(x)) - boardX) / cellWidth)));
+                    const by = @as(u8, @intFromFloat(@floor((@as(f32, @floatFromInt(y)) - boardY) / cellHeight)));
+
+                    assert(bx >= 0 and by >= 0);
+                    const index: usize = @intCast(by * numColumns + bx);
+
+                    if (board[index] == null) {
+                        // Make a move
+                        board[index] = turn;
+                        numEmptyCells -= 1;
+
+                        //const isGameOver = getGameResult(board[0..], ...);
+                        //if (isGameOver.yap) {
+                        //    switch (isGameOver.result) {
+                        //        .WIN => {
+                        //            gameState.gameOn = false;
+                        //        },
+                        //        .DRAW => {
+                        //            gameState.gameOn = false;
+                        //        },
+                        //    }
+                        //} else {
+                        //    // switch turn
+                        //}
+
+                        if (isWin(board[0..], @intCast(bx), @intCast(by), numColumns)) {
+                            gameOver = true;
+
+                            const allocator = std.heap.page_allocator;
+                            gameOverString = try std.fmt.allocPrint(allocator, "{s} wins!", .{stoneToString[@intFromEnum(turn)]});
+
+                            print("WIN {s}\n", .{stoneToString[@intFromEnum(turn)]});
+                            //for (&board) |*cell| {
+                            //    cell.* = 0;
+                            //}
+                        } else if (numEmptyCells == 0) {
+                            gameOver = true;
+                            print("DRAW\n", .{});
                         } else {
-                            print("Can't place a stone there!\n", .{});
+                            turn = if (turn == .BLACK) .WHITE else .BLACK;
                         }
+                    } else {
+                        print("Can't place a stone there!\n", .{});
                     }
                 }
-
-                wasDown = true;
-            } else {
-                wasDown = false;
             }
         }
 
@@ -113,170 +132,214 @@ pub fn main() !void {
         const shortenBy = 10;
         const startY = boardY + shortenBy;
         const endY = boardY + boardHeight - shortenBy;
-        for (1..3) |i| {
-            const x: i32 = boardX + @as(i32, @intCast(i)) * cellWidth;
+        for (1..numColumns) |i| {
+            const x: i32 = @as(i32, @intFromFloat(boardX)) + @as(i32, @intCast(i)) * @as(i32, @intFromFloat(cellWidth));
             ray.DrawLine(x, startY, x, endY, ray.BLACK);
         }
 
         // Draw horizontal lines
         const startX = boardX;
         const endX = boardX + boardWidth;
-        for (1..3) |i| {
-            const y: i32 = boardY + @as(i32, @intCast(i)) * cellHeight;
+        for (1..numRows) |i| {
+            const y: i32 = @as(i32, @intFromFloat(boardY)) + @as(i32, @intCast(i)) * @as(i32, @intFromFloat(cellHeight));
             ray.DrawLine(startX, y, endX, y, ray.BLACK);
         }
 
         // Draw stones
-        for(board, 0..) |cell, j| {
-            if (cell == 0) continue;
+        for (board, 0..) |cell, j| {
+            if (cell == null) continue;
 
-            const i: u16 = @intCast(j);
-            const x = i % columnsInRow;
-            const y = i / columnsInRow;
-            const stoneX = boardX + x * cellWidth + cellWidth / 2;
-            const stoneY = boardY + y * cellHeight + cellHeight / 2;
+            const i: u8 = @intCast(j);
+            const x: f32 = @floatFromInt(i % numColumns);
+            const y: f32 = @floatFromInt(i / numColumns);
+            const stoneX: i32 = @intFromFloat(@round(boardX + x * cellWidth + cellWidth / 2));
+            const stoneY = @as(i32, @intFromFloat(@round(boardY + y * cellHeight + cellHeight / 2)));
             const stoneRadius = cellWidth / 3;
-            const stoneColor = if(cell == 1) ray.BLACK else ray.WHITE;
+            const stoneColor = if(cell == .BLACK) ray.BLACK else ray.WHITE;
             ray.DrawCircle(stoneX, stoneY, stoneRadius, stoneColor);
         }
 
         if (gameOver) {
-            ray.DrawText("Game over", 10, 10, 24, ray.BLACK);
+            if (gameOverString) |string| {
+                ray.DrawText(string.ptr, 10, 10, 24, ray.BLACK);
+            } else {
+                ray.DrawText("Draw!", 10, 10, 24, ray.BLACK);
+            }
         }
 
         ray.EndDrawing();
     }
 }
 
-fn isWin(board: []i8, lastMoveX: i8, lastMoveY: i8, columnsInRow: u8) bool {
-    const lastMoveStone = board[@intCast(lastMoveY * @as(i8, @intCast(columnsInRow)) + lastMoveX)];
-    assert(lastMoveStone == 1 or lastMoveStone == -1);
+fn isWin(board: []?Stone, lastMoveX: i8, lastMoveY: i8, columnsInRow: u8) bool {
+    const lastMoveStone = board[getIndex(lastMoveX, lastMoveY, columnsInRow)];
+    print("type lastMoveStone: {}\n", .{@TypeOf(lastMoveStone)});
+    //assert(lastMoveStone == .WHITE or lastMoveStone == .BLACK);
+    assert(lastMoveStone != null);
 
-    var stoneCount: usize = undefined;
+    //var stoneCount: usize = undefined;
 
-    stoneCount = 1;
-    {
-        var dx: i8 = 1;
-        while (true) : (dx += 1) {
-            const x: isize = lastMoveX + dx;
-            const y: isize = lastMoveY;
+    const rows: [4][2]struct{dx: i8, dy: i8} = .{
+        .{.{.dx = 1, .dy = 0}, .{.dx = -1, .dy = 0}}, // horizontal (right, left)
+        .{.{.dx = 0, .dy = 1}, .{.dx = 0, .dy = -1}}, // vertical (down, up)
+        .{.{.dx = 1, .dy = 1}, .{.dx = -1, .dy = -1}}, // 1st diagonal
+        .{.{.dx = 1, .dy = -1}, .{.dx = -1, .dy = 1}}, // 2nd diagonal
+    };
+    _ = rows[0];
+    for (rows) |directions| {
+        print("{}\n", .{@TypeOf(directions)});
+        var stoneCount: u8 = 1;
+        for (directions) |direction| {
+            print("dx: {}, dy: {}\n", .{direction.dx, direction.dy});
 
-            if (x > 2) break;
-            if (board[@intCast(y * columnsInRow + x)] != lastMoveStone) break;
+            var dx: i8 = direction.dx;
+            var dy: i8 = direction.dy;
+            while (true) : ({dx += direction.dx; dy += direction.dy;}) {
+                const x = lastMoveX + dx;
+                const y = lastMoveY + dy;
 
-            stoneCount += 1;
+                if (x < 0 or x > 2 or y < 0 or y > 2) break;
+                if (board[getIndex(x, y, columnsInRow)] != lastMoveStone) break;
+
+                stoneCount += 1;
+            }
+        }
+        
+        assert(stoneCount <= 3);
+        if (stoneCount == 3) {
+            return true;
         }
     }
-    {
-        var dx: i8 = -1;
-        while (true) : (dx -= 1) {
-            const x: isize = lastMoveX + dx;
-            const y: isize = lastMoveY;
-
-            if (x < 0) break;
-            if (board[@intCast(y * columnsInRow + x)] != lastMoveStone) break;
-
-            stoneCount += 1;
-        }
-    }
-
-    if (stoneCount == 3) {
-        return true;
-    }
-
-    stoneCount = 1;
-    {
-        var dy: i8 = 1;
-        while (true) : (dy += 1) {
-            const x: isize = lastMoveX;
-            const y: isize = lastMoveY + dy;
-
-            if (y > 2) break;
-            if (board[@intCast(y * columnsInRow + x)] != lastMoveStone) break;
-
-            stoneCount += 1;
-        }
-    }
-    {
-        var dy: i8 = -1;
-        while (true) : (dy -= 1) {
-            const x: isize = lastMoveX;
-            const y: isize = lastMoveY + dy;
-
-            if (y < 0) break;
-            if (board[@intCast(y * columnsInRow + x)] != lastMoveStone) break;
-
-            stoneCount += 1;
-        }
-    }
-
-    if (stoneCount == 3) {
-        return true;
-    }
-
-    stoneCount = 1;
-    {
-        var dx: i8 = 1;
-        var dy: i8 = 1;
-        while (true) : ({dx += 1; dy += 1;}) {
-            const x: isize = lastMoveX + dx;
-            const y: isize = lastMoveY + dy;
-
-            if (x > 2 or y > 2) break;
-            if (board[@intCast(y * columnsInRow + x)] != lastMoveStone) break;
-
-            stoneCount += 1;
-        }
-    }
-    {
-        var dx: i8 = -1;
-        var dy: i8 = -1;
-        while (true) : ({dx -= 1; dy -= 1;}) {
-            const x: isize = lastMoveX + dx;
-            const y: isize = lastMoveY + dy;
-
-            if (x < 0 or y < 0) break;
-            if (board[@intCast(y * columnsInRow + x)] != lastMoveStone) break;
-
-            stoneCount += 1;
-        }
-    }
-
-    if (stoneCount == 3) {
-        return true;
-    }
-
-    stoneCount = 1;
-    {
-        var dx: i8 = 1;
-        var dy: i8 = -1;
-        while (true) : ({dx += 1; dy -= 1;}) {
-            const x: isize = lastMoveX + dx;
-            const y: isize = lastMoveY + dy;
-
-            if (x > 2 or y < 0) break;
-            if (board[@intCast(y * columnsInRow + x)] != lastMoveStone) break;
-
-            stoneCount += 1;
-        }
-    }
-    {
-        var dx: i8 = -1;
-        var dy: i8 = 1;
-        while (true) : ({dx -= 1; dy += 1;}) {
-            const x: isize = lastMoveX + dx;
-            const y: isize = lastMoveY + dy;
-
-            if (x < 0 or y > 2) break;
-            if (board[@intCast(y * columnsInRow + x)] != lastMoveStone) break;
-
-            stoneCount += 1;
-        }
-    }
-
-    if (stoneCount == 3) {
-        return true;
-    }
-
     return false;
+
+    //stoneCount = 1;
+    //{
+    //    var dx: i8 = 1;
+    //    while (true) : (dx += 1) {
+    //        const x = lastMoveX + dx;
+    //        const y = lastMoveY;
+
+    //        if (x > 2) break;
+    //        if (board[getIndex(x, y, columnsInRow)] != lastMoveStone) break;
+
+    //        stoneCount += 1;
+    //    }
+    //}
+    //{
+    //    var dx: i8 = -1;
+    //    while (true) : (dx -= 1) {
+    //        const x: isize = lastMoveX + dx;
+    //        const y: isize = lastMoveY;
+
+    //        if (x < 0) break;
+    //        if (board[@intCast(y * columnsInRow + x)] != lastMoveStone) break;
+
+    //        stoneCount += 1;
+    //    }
+    //}
+
+    //assert(stoneCount <= 3);
+    //if (stoneCount == 3) {
+    //    return true;
+    //}
+
+    //stoneCount = 1;
+    //{
+    //    var dy: i8 = 1;
+    //    while (true) : (dy += 1) {
+    //        const x: isize = lastMoveX;
+    //        const y: isize = lastMoveY + dy;
+
+    //        if (y > 2) break;
+    //        if (board[@intCast(y * columnsInRow + x)] != lastMoveStone) break;
+
+    //        stoneCount += 1;
+    //    }
+    //}
+    //{
+    //    var dy: i8 = -1;
+    //    while (true) : (dy -= 1) {
+    //        const x: isize = lastMoveX;
+    //        const y: isize = lastMoveY + dy;
+
+    //        if (y < 0) break;
+    //        if (board[@intCast(y * columnsInRow + x)] != lastMoveStone) break;
+
+    //        stoneCount += 1;
+    //    }
+    //}
+
+    //if (stoneCount == 3) {
+    //    return true;
+    //}
+
+    //stoneCount = 1;
+    //{
+    //    var dx: i8 = 1;
+    //    var dy: i8 = 1;
+    //    while (true) : ({dx += 1; dy += 1;}) {
+    //        const x: isize = lastMoveX + dx;
+    //        const y: isize = lastMoveY + dy;
+
+    //        if (x > 2 or y > 2) break;
+    //        if (board[@intCast(y * columnsInRow + x)] != lastMoveStone) break;
+
+    //        stoneCount += 1;
+    //    }
+    //}
+    //{
+    //    var dx: i8 = -1;
+    //    var dy: i8 = -1;
+    //    while (true) : ({dx -= 1; dy -= 1;}) {
+    //        const x: isize = lastMoveX + dx;
+    //        const y: isize = lastMoveY + dy;
+
+    //        if (x < 0 or y < 0) break;
+    //        if (board[@intCast(y * columnsInRow + x)] != lastMoveStone) break;
+
+    //        stoneCount += 1;
+    //    }
+    //}
+
+    //if (stoneCount == 3) {
+    //    return true;
+    //}
+
+    //stoneCount = 1;
+    //{
+    //    var dx: i8 = 1;
+    //    var dy: i8 = -1;
+    //    while (true) : ({dx += 1; dy -= 1;}) {
+    //        const x: isize = lastMoveX + dx;
+    //        const y: isize = lastMoveY + dy;
+
+    //        if (x > 2 or y < 0) break;
+    //        if (board[@intCast(y * columnsInRow + x)] != lastMoveStone) break;
+
+    //        stoneCount += 1;
+    //    }
+    //}
+    //{
+    //    var dx: i8 = -1;
+    //    var dy: i8 = 1;
+    //    while (true) : ({dx -= 1; dy += 1;}) {
+    //        const x: isize = lastMoveX + dx;
+    //        const y: isize = lastMoveY + dy;
+
+    //        if (x < 0 or y > 2) break;
+    //        if (board[@intCast(y * columnsInRow + x)] != lastMoveStone) break;
+
+    //        stoneCount += 1;
+    //    }
+    //}
+
+    //if (stoneCount == 3) {
+    //    return true;
+    //}
+
+    //return false;
+}
+
+fn getIndex(x: i8, y: i8, columnsInRow: u8) usize {
+    return @intCast(y * @as(i8, @intCast(columnsInRow)) + x);
 }
